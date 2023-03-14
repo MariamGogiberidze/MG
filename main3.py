9c5321ec8c09e4062d2dced41f63a3c07cbb7a41
MariamGogiberidze <127139775+MariamGogiberidze@users.noreply.github.com>
2023-03-14 20:05:11 +0400

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+if name == "Khatia":
+    print("Nice to hear from you")
+elif name == "Nino":
+    print("Hello my dear")
+elif name == "Keti":
+    print("Hi my friend?")
+   
